e928fa4b82fff7ab9f2ce90eb6d5878f1bccbbef
blackip <kkoodd2012@gmail.com>
2020-06-12 14:29:16 +0700

    connect to DB

--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const dotenv = require("dotenv");
 //const logger = require("./middleware/logger");
 const morgan = require("morgan"); //Middleware for logger
-
-// Route files
-const bootcamps = require("./routes/bootcamp");
+const connectDB = require("./config/db");
+const colors = require("colors");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to DB
+connectDB();
+
+// Route files
+const bootcamps = require("./routes/bootcamp");
 
 const app = express();
 
@@ -25,7 +30,17 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .inverse
+  )
 );
+
+// Handle unhandle promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error:  ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
