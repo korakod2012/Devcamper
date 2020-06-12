d8600f98d7a6dc42a2c74df7862e094e8a6598b5
blackip <kkoodd2012@gmail.com>
2020-06-12 09:28:31 +0700

    Start Use Git

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Route files
+const bootcamps = require("./routes/bootcamp");
+
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
+app.get("/", (req, res) => {
+  res.send("hello from express");
+});
 
 const PORT = process.env.PORT || 5000;
 
